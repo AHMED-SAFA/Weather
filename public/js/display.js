2abe17108d53d1d636b125f874ef4df2417a0581
safa <ahmedsafa0759@gmail.com>
2024-08-19 22:01:00 +0600

    frontend updated

---
+++ public/js/display.js
@@ -0,0 +1,5 @@
+var weatherapi = "/weather";
+const weatherForm = document.querySelector('form');
+const weatherInput = document.querySelector('input');
+const weatherForm = document.querySelector('form');
+const weatherForm = document.querySelector('form');
